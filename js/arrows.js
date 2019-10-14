9a8fc60fa2926068d693826fef82a6e9f248b4a9
Dr. Tobias Schröder <tobias.schroeder@metronom.com>
2019-10-14 10:23:16 +0200

    TS: Init

---
+++ js/arrows.js
@@ -0,0 +1,35 @@
+var c=document.getElementById("canvas1");
+var ctx=c.getContext("2d");
+ctx.beginPath(); 
+ctx.lineWidth="3";
+ctx.strokeStyle="white"; 
+ctx.moveTo(0,0);
+ctx.lineTo(110, 200);
+ctx.stroke();
+
+var c=document.getElementById("canvas2");
+var ctx=c.getContext("2d");
+ctx.beginPath(); 
+ctx.lineWidth="3";
+ctx.strokeStyle="white"; 
+ctx.moveTo(249,20);
+ctx.lineTo(2,68);
+ctx.stroke();
+
+var c=document.getElementById("canvas3");
+var ctx=c.getContext("2d");
+ctx.beginPath(); 
+ctx.lineWidth="3";
+ctx.strokeStyle="white"; 
+ctx.moveTo(180,0);
+ctx.lineTo(0,250);
+ctx.stroke();
+
+var c=document.getElementById("canvas4");
+var ctx=c.getContext("2d");
+ctx.beginPath(); 
+ctx.lineWidth="3";
+ctx.strokeStyle="white"; 
+ctx.moveTo(285,5);
+ctx.lineTo(5,25);
+ctx.stroke();
